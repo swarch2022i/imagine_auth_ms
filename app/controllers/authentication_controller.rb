1078ae63c75b7b23d82e6e1b2ee91a0e7cc64bb8
Camilo <dc.perezc1@uniandes.edu.co>
2022-06-09 00:37:07 -0500

    ldap

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,12 +5,19 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by_username!(params[:username])
         if @user&.authenticate(params[:password])
-            payload = {user_id: @user.id, username: @user.username}
-            token = JsonWebToken.encode(payload)
-            time = Time.now + 24.hours.to_i
-            render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                        username: @user.username ,
-                        id: @user.id}, status: :ok
+            ldap = Net::LDAP.new(:host => ENV['LDAP_HOST'], :port => 389)
+            if ldap.bind(:method => :simple, :username => "cn=#{params[:username]},ou=auth,dc=imagine,dc=unal,dc=edu,dc=co",
+                :password => params[:password])
+
+                payload = {user_id: @user.id, username: @user.username}
+                token = JsonWebToken.encode(payload)
+                time = Time.now + 24.hours.to_i
+                render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                            username: @user.username ,
+                            id: @user.id}, status: :ok
+            else
+                render json: { error: "#{ldap.get_operation_result}" }, status: :unauthorized
+            end
         else
             render json: { error: 'unauthorized' }, status: :unauthorized
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,30 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-        render json: @user, status: :created
+            dn = "cn=#{params[:username]},ou=auth,dc=imagine,dc=unal,dc=edu,dc=co"
+            cryptPassword = params[:password]
+            attr = {
+                :cn => params[:username],
+                :sn => params[:username],
+                :uid => params[:username],
+                :objectclass => ["inetOrgPerson", "top", "posixAccount"],
+                :gidNumber => "500",
+                :uidNumber => "#{1000 + @user.id}",
+                :userPassword => cryptPassword,
+                :homeDirectory => "/home/users/#{params[:username]}"
+            }
+            ldap = Net::LDAP.new :host => ENV['LDAP_HOST'],
+                :port => 389,
+                :auth => {
+                    :method => :simple,
+                    :username => "cn=admin,dc=imagine,dc=unal,dc=edu,dc=co",
+                    :password => "admin"
+            }
+            ldap.add(:dn => dn, :attributes => attr)
+            render json: @user, status: :created
         else
-        render json: { errors: @user.errors.full_messages },
-                status: :unprocessable_entity
+            render json: { errors: @user.errors.full_messages },
+                    status: :unprocessable_entity
         end
     end
 
